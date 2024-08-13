57ed19b20281b97831a97ac439f219734bec0084
unknown <jroserog@itsqmet.edu.ec>
2024-08-13 11:10:30 -0500

    actualziacion

--- main.py
+++ main.py
@@ -2,19 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 import secrets as st
 
 from time import sleep
+import os
+
+##############################################################################
+def create_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+def set_download_folder(driver, folder_path):
+    # Cambia las preferencias de descarga para que se guarde en una carpeta específica
+    driver.execute_cdp_cmd('Page.setDownloadBehavior', {
+        'behavior': 'allow',
+        'downloadPath': folder_path
+    })
+########################################################################################
 
 driver = webdriver.Chrome()
+
+#driver.maximize_window()  #MAXIMIsA PANTALLA
 driver.get("https://campusvirtual.itsqmet.edu.ec/campusV")
 sleep(1.5)
 
 botonIngresar = driver.find_element(By.XPATH, "/html/body/div[1]/main/div/div/div[2]/div[2]/a")
 botonIngresar.click()
-sleep(4)
+sleep(3)
 
 #Ingreso de credenciales
 email = driver.find_element(By.XPATH, '//*[@id="i0116"]')
@@ -51,11 +68,34 @@ select = Select(select_element)
 #select.select_by_value('OCTUBRE 202')
 
 select.select_by_index(9)
+sleep(12)
+select_elementM = driver.find_element(By.XPATH, '//*[@id="ContentPlaceHolder1_ddlAulasVirtuales"]')
+selectM = Select(select_elementM)
+
+opciones = selectM.options
+
+for indice in range(len(opciones)):
+    print(indice)
+    selectM.select_by_index(indice)
+    sleep(45)
+
+    ##SELECCIONAR ÁREA REPORTE FINAL
+    reporte = driver.find_element(By.XPATH, '//*[@id="__tab_ContentPlaceHolder1_TabContainer1_tabInfFinAsig"]/span')
+    reporte.click()
+    sleep(1)
 
 
+    # Descargar
+    descargar = driver.find_element(By.XPATH, '//*[@id="ContentPlaceHolder1_TabContainer1_tabInfFinAsig_gdInfFinAsig_btnImprimirReporteFinAsig_0"]')
+    descargar.click()
+    sleep(15)
 
-##SELECCIONAR REPORTE FINAL
-sleep(15)
-reporte = driver.find_element(By.XPATH, '//*[@id="__tab_ContentPlaceHolder1_TabContainer1_tabInfFinAsig"]')
-reporte.click()
+    ########################################
+
 sleep(1000)
+
+
+
+
+
+
